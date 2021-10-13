12626ab63c4bff4ca82863a388991977b9f41e14
Peter Muessig <peter.muessig@sap.com>
2021-10-13 09:56:49 +0200

    Consume apollo-bundle in UI5 application

---
+++ packages/sap.ui.demo.apollo-lib/bundle/index.ts
@@ -0,0 +1,13 @@
+// https://www.apollographql.com/docs/react/get-started/
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql
+} from "@apollo/client";
+
+export default {
+    ApolloClient,
+    gql
+}
